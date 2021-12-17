c2f65746ca8734ca551cd7e61485c413ae842ee6
asiancart <asiancart@hotmail.com>
2021-12-17 12:03:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+choices = ["rock","paper","scissors"]
+
+computer =random.choice(choices)
+player = None
+
+while player not in choices:
+    player = input("rock,paper or scissors: ").lower()
+
+print("computer:",computer)
+print("player:",player)
+
+if player == computer:
+    print("successful")
+else:
+    print("wrong")
